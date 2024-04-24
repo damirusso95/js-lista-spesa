07f9bb60c1851672f62c3b4d4abdd7fe3a813c87
damirusso95 <damirusso95@gmail.com>
2024-04-24 15:28:31 +0200

    bonus- stampo lista con innerhtml

--- Nuova cartella/main.js
+++ Nuova cartella/main.js
@@ -7,10 +7,16 @@ const lista = [
 ];
 console.log(lista);
 
+// seleziono la ul
+const listaElement = document.querySelector("ul");
+
 // ciclo for itero gli elementi
 console.log("Itero gli elementi con for");
 for (let i = 0; i < lista.length; i++) {
 
 	const element = lista[i];
 	console.log(i, element);
+
+// stampo tanti "li" quanti sono gli elementi nella lista
+    listaElement.innerHTML += `<li>${element}</li>`;
 }
