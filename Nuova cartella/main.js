e3e4f08a1257a72aa883d56de6773875093b6750
damirusso95 <damirusso95@gmail.com>
2024-04-24 15:33:27 +0200

    M2-M3: conversione ciclo da for a while

--- Nuova cartella/main.js
+++ Nuova cartella/main.js
@@ -14,9 +14,29 @@ const listaElement = document.querySelector("ul");
 console.log("Itero gli elementi con for");
 for (let i = 0; i < lista.length; i++) {
 
-	const element = lista[i];
-	console.log(i, element);
+    const element = lista[i];
+    console.log(i, element);
 
-// stampo tanti "li" quanti sono gli elementi nella lista
+    // stampo tanti "li" quanti sono gli elementi nella lista
     listaElement.innerHTML += `<li>${element}</li>`;
 }
+
+
+
+// ciclo while
+
+
+console.log("Itero gli elementi con while");
+// definisco i fuori dal ciclo
+let i = 0;
+// inizio ciclo while
+while (i < lista.length) {
+
+    const element = lista[i];
+    console.log(i, element);
+
+    i++;
+}
+// resetto l'indice e lo porto nuovamente alla condizione iniziale = 0
+i = 0;
+console.log("Contatore ancora esistente dopo il while ma resettato: ", i);
